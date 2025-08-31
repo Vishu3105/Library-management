eb427402b7e1990dee124e23510f9d11d40c13af
Vishu3105 <vishwass2003@gmail.com>
2025-08-31 22:38:19 +0530

    added issuedTo feature

--- src/Library.java
+++ src/Library.java
@@ -14,19 +14,23 @@ class Library {
 
     public void showAvailableBooks() {
         System.out.println("\nAvailable Books:");
+        boolean found = false;
         for (Book book : books) {
             if (!book.isIssued()) {
                 System.out.println(book);
+                found = true;
             }
+        }
+        if (!found) {
+            System.out.println("No books available.");
         }
     }
 
     public void issueBook(String title, User user) {
         for (Book book : books) {
             if (book.getTitle().equalsIgnoreCase(title) && !book.isIssued()) {
-                book.issueBook();
                 user.issueBook(book);
-                System.out.println(user.getName() + " issued " + book.getTitle());
+                System.out.println(book.getTitle() + " issued to" + user.getName());
                 return;
             }
         }
@@ -36,13 +40,26 @@ class Library {
     public void returnBook(String title, User user) {
         for (Book book : user.getIssuedBooks()) {
             if (book.getTitle().equalsIgnoreCase(title)) {
-                book.returnBook();
                 user.returnBook(book);
                 System.out.println(user.getName() + " returned " + book.getTitle());
                 return;
             }
         }
         System.out.println("This book was not issued by " + user.getName());
+    }
+
+    public void showIssuedBooks() {
+        System.out.println("\nIssued Books:");
+        boolean found = false;
+        for (Book book : books) {
+            if (book.isIssued()) {
+                System.out.println(book.getTitle() + " issued to " + book.getIssuedTo().getName());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No books are currently issued.");
+        }
     }
 }
 
