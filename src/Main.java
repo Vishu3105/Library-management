3dc2ed321b4a6a91a6eba724fe772afd40791462
Vishu3105 <vishwass2003@gmail.com>
2025-08-31 23:08:26 +0530

    added multiple users

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Library library = new Library();
-        User user = new User("Vishwas"); // static single user for now
+        HashMap<String, User> users = new HashMap<>(); // multi users
 
         while (true) {
             System.out.println("\n--- Library Menu ---");
@@ -12,7 +13,8 @@ public class Main {
             System.out.println("2. Show Available Books");
             System.out.println("3. Issue Book");
             System.out.println("4. Return Book");
-            System.out.println("5. Exit");
+            System.out.println("5. Show All Issued Books");
+            System.out.println("6. Exit");
             System.out.print("Choose option: ");
 
             int choice = sc.nextInt();
@@ -32,18 +34,33 @@ public class Main {
                     break;
 
                 case 3:
+                    System.out.print("Enter student name: ");
+                    String issueUserName = sc.nextLine();
+                    users.putIfAbsent(issueUserName, new User(issueUserName));
+                    User issueUser = users.get(issueUserName);
                     System.out.print("Enter book title to issue: ");
                     String issueTitle = sc.nextLine();
-                    library.issueBook(issueTitle, user);
+                    library.issueBook(issueTitle, issueUser);
                     break;
 
                 case 4:
+                    System.out.print("Enter student name: ");
+                    String returnUserName = sc.nextLine();
+                    if (!users.containsKey(returnUserName)) {
+                        System.out.println("No such student.");
+                        break;
+                    }
+                    User returnUser = users.get(returnUserName);
                     System.out.print("Enter book title to return: ");
                     String returnTitle = sc.nextLine();
-                    library.returnBook(returnTitle, user);
+                    library.returnBook(returnTitle, returnUser);
                     break;
 
                 case 5:
+                    library.showIssuedBooks();
+                    break;
+
+                case 6:
                     System.out.println("Exiting...");
                     sc.close();
                     return;
