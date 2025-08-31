5abd8f8202ef859a47c57cf4f3c9e006fba75eac
Vishu3105 <vishwass2003@gmail.com>
2025-08-31 20:50:40 +0530

    init

--- src/Book.java
+++ src/Book.java
@@ -2,23 +2,48 @@ class Book {
     private String title;
     private String author;
     private boolean isIssued;
+    private User issuedTo;
 
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
         this.isIssued = false;
+        this.issuedTo = null;
     }
 
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public boolean isIssued() { return isIssued; }
+    public String getTitle() {
+        return title;
+    }
 
-    public void issueBook() { isIssued = true; }
-    public void returnBook() { isIssued = false; }
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public User getIssuedTo() {
+        return issuedTo;
+    }
+
+    public void issueBook(User user) {
+        isIssued = true;
+        issuedTo = user;
+    }
+
+    public void returnBook() {
+        isIssued = false;
+        issuedTo = null;
+    }
 
     @Override
     public String toString() {
-        return title + " by " + author + (isIssued ? " [Issued]" : " [Available]");
+        if (isIssued) {
+            return title + " by " + author + " [Issued to: " + issuedTo.getName() + "]";
+        } else {
+            return title + " by " + author + " [Available]";
+        }
     }
 }
 
